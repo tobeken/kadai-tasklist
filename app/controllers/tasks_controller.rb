7a2192e800ada341f9e5d3f201ff8ff7b6a60bee
ken.tobioka <tobik0124@gmail.com>
2020-12-13 10:36:40 +0000

    change

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -43,7 +43,7 @@ class TasksController < ApplicationController
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
-add 
+    
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
