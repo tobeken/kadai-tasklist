c2b48d80f90bee3de4f250eb063961a7c556c6be
ken.tobioka <tobik0124@gmail.com>
2021-01-08 15:42:29 +0000

    error

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
 before_action :require_user_logged_in, only: [:index, :show]
+before_action :correct_user, only:[:show, :edit, :destroy]
 
   def index
-     @tasks = Task.all
+      if logged_in?
+      @task = current_user.tasks.build  
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      end
   end
 
   def show
@@ -14,14 +18,14 @@ before_action :require_user_logged_in, only: [:index, :show]
   end
 
   def create
-       @task = Task.new(task_params)
-
+   @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      flash[:success] = 'タスクを投稿しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'タスクの投稿に失敗しました。'
+    
     end
   end
 
@@ -42,11 +46,9 @@ before_action :require_user_logged_in, only: [:index, :show]
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-    
-    flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    flash[:success] = 'タスクを削除しました。'
+    redirect_back(fallback_location: root_path)
   end
   
   private
@@ -54,6 +56,13 @@ before_action :require_user_logged_in, only: [:index, :show]
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-   before_action :require_user_logged_in, only: [:index, :show]
- 
+ before_action :require_user_logged_in, only: [:index, :show]
+
   def index
     @users = User.order(id: desc).page(params[:page]).per(25)
   end
@@ -14,16 +14,7 @@ class UsersController < ApplicationController
   end
 
   def create
-     @user = User.new(user_params)
-
-    if @user.save
-      flash[:success] = 'ユーザを登録しました。'
-      session[:user_id] = @user.id
-      redirect_to root_url
-    else
-      flash.now[:danger] = 'ユーザの登録に失敗しました。'
-      render :new
-    end
+    
   end
   
   private
