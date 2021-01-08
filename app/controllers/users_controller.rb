810598237828464c0963ed5ed5d8c2bf93633783
ken.tobioka <tobik0124@gmail.com>
2021-01-08 12:59:01 +0000

    migration前

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+   before_action :require_user_logged_in, only: [:index, :show]
+ 
   def index
     @users = User.order(id: desc).page(params[:page]).per(25)
   end
@@ -16,7 +18,8 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      session[:user_id] = @user.id
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: 'tasks#index'
 
-root to: 'toppages#index'
-
-  get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  get 'logout', to: 'sessions#destroy'
-  resources :tasks, only: [:index, :show, :new, :create]
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+
+  resources :users, only: [ :new, :create]
+  resources :tasks
 end
