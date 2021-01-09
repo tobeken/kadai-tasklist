421689a907f91f1745f69a585b0f8b71691c1a20
ken.tobioka <tobik0124@gmail.com>
2021-01-09 15:42:31 +0000

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
   end
   
   private
